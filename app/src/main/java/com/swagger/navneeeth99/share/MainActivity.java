cddbd5d88a5e752622ff3c248637fbf6261d635b
DudeSurfer <navneeth.k50@gmail.com>
2015-06-22 23:48:18 +0800

    Preliminary side nav tests

--- app/src/main/java/com/swagger/navneeeth99/share/MainActivity.java
+++ app/src/main/java/com/swagger/navneeeth99/share/MainActivity.java
@@ -1,17 +1,53 @@
 package com.swagger.navneeeth99.share;
 
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
+    private String[] mNavChoices;
+    private DrawerLayout mLeftNavDrawer;
+    private android.support.v4.app.ActionBarDrawerToggle mNavToggle;
+    private ListView mLeftNavList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNavChoices = getResources().getStringArray(R.array.navdrawer_items);
+        mLeftNavDrawer = (DrawerLayout)findViewById(R.id.side_nav);
+        mLeftNavList = (ListView)findViewById(R.id.drawer_list);
+
+        mLeftNavList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mNavChoices));
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mNavToggle = new android.support.v4.app.ActionBarDrawerToggle(this, mLeftNavDrawer, R.drawable.ic_drawer, R.string.open, R.string.close) {
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                //getActionBar().setTitle(mTitle);
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                //getActionBar().setTitle(mDrawerTitle);
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mLeftNavDrawer.setDrawerListener(mNavToggle);
     }
 
 
@@ -34,6 +70,24 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
 
+        if (mNavToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mNavToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mNavToggle.onConfigurationChanged(newConfig);
+    }
+
 }
