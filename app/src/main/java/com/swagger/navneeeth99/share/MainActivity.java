17adfb934f400adae97117f69c63645fac52ead3
DudeSurfer <navneeth.k50@gmail.com>
2015-06-24 23:02:06 +0800

    Notes making works
    tried to fix crash too but not sure

--- app/src/main/java/com/swagger/navneeeth99/share/MainActivity.java
+++ app/src/main/java/com/swagger/navneeeth99/share/MainActivity.java
@@ -22,16 +22,15 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        super.onCreateDrawer();
 
         ParseUser currentUser = ParseUser.getCurrentUser();
         if(currentUser==null){
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
         } else {
-            super.onCreateDrawer();
+            //Do everything else here
         }
-
-
     }
 
 
