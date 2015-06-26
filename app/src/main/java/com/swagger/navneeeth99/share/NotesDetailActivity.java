7e55bedcdbdc6e7e941ea1a41a146854bdcd11a4
ChampBenRockz <seraphi123@yahoo.com.sg>
2015-06-26 19:17:05 +0800

    Added NotesDetailActivity and its layout

---
+++ app/src/main/java/com/swagger/navneeeth99/share/NotesDetailActivity.java
@@ -0,0 +1,17 @@
+package com.swagger.navneeeth99.share;
+
+import android.os.Bundle;
+
+/**
+ * Created by Benjamin on 26/6/15.
+ */
+public class NotesDetailActivity extends BaseActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes_detail);
+        super.onCreateDrawer();
+
+
+    }
+}
